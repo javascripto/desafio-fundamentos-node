f7a82b1f3afb33eb5e1b4b9652edfb0933b43330
Yuri Almeida <yurialves2@gmail.com>
2020-07-01 00:13:44 -0300

    created request handlers for routes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance()
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,9 +19,19 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
+    const { title, value, type } = request.body;
+    const parsedValue = Number(value);
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository
+    );
+    const transaction = createTransaction.execute({
+      title,
+      type,
+      value: parsedValue,
+    });
+    return response.json(transaction);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
   }
 });
 
